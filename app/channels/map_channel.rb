b2f108a86e256b4b3260484e77bf12b9b7e3fc9a
Yigal Saperstein <yigal.saperstein@macaulay.cuny.edu>
2017-01-18 22:19:55 +0200

    bla

--- app/channels/map_channel.rb
+++ app/channels/map_channel.rb
@@ -1,6 +1,7 @@
 class MapChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_for current_user
   end
 
   def unsubscribed
@@ -9,5 +10,12 @@ class MapChannel < ApplicationCable::Channel
 
   def locate (data)
     puts (data)
+
+
+    # Somewhere in your app this is called, perhaps from a NewCommentJob
+    MapChannel.broadcast_to(
+        current_user,
+        objects: Coin.within(0.185, :origin => [32.064510, 34.774284]).as_json(only: [:id, :money_value, :lat, :lng])
+    )
   end
 end
