b86e5f2fc19961a9ad2dbd7e8d0fb89df630681b
Yigal Saperstein <yigal.saperstein@macaulay.cuny.edu>
2017-01-18 14:43:21 +0200

    heroku setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,12 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.web_socket_server_url = "wss://changetocharity.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://changetocharity.herokuapp.com', 'http://changetocharity.herokuapp.com']
+
+
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
